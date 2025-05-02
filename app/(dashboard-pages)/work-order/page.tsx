c13ba16aefce57f1a9c7f811b6b5bdcd6264d878
fridoustin <fridolinaustin16@gmail.com>
2025-05-02 13:11:36 +0800

    fix: fix bug when filtering

--- app/(dashboard-pages)/work-order/page.tsx
+++ app/(dashboard-pages)/work-order/page.tsx
@@ -2,13 +2,13 @@ import WorkOrderList from "./WOList";
 
 const workOrders = [
   { id: 1, title: "AC Rusak", status: "Waiting" },
-  { id: 2, title: "Lampu Mati", status: "Procces" },
+  { id: 2, title: "Lampu Mati", status: "Process" },
   { id: 3, title: "Meja Pecah", status: "Finish" },
   { id: 4, title: "Perlu Dibersihkan", status: "Waiting" },
-  { id: 5, title: "Kursi Rusak", status: "Procces" },
+  { id: 5, title: "Kursi Rusak", status: "Process" },
   { id: 6, title: "Pintu Macet", status: "Finish" },
-  { id: 7, title: "Ruangan Berdebu", status: "Procces" },
-  { id: 8, title: "AC Bocor", status: "procces" },
+  { id: 7, title: "Ruangan Berdebu", status: "Process" },
+  { id: 8, title: "AC Bocor", status: "Process" },
   { id: 9, title: "Monitor Tidak Dapat Nyala", status: "Finish" },
 ];
 
--- components/ui/WorkOrderCard.tsx
+++ components/ui/WorkOrderCard.tsx
@@ -12,13 +12,25 @@ interface WorkOrderCardProps {
 }
 
 export default function WorkOrderCard({ data, onViewDetail }: WorkOrderCardProps) {
+  const getStatusColor = (status: string) => {
+    switch (status.toLowerCase()) {
+      case "finish":
+        return "text-green-600 dark:text-green-400";
+      case "process":
+        return "text-yellow-600 dark:text-yellow-400";
+      case "canceled":
+        return "text-red-600 dark:text-red-400";
+      default:
+        return "text-gray-600 dark:text-gray-300";
+    }
+  };
   return (
-    <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-xl p-4 shadow hover:shadow-lg transition-all">
+    <div className=" w-[270px] bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-xl p-4 shadow hover:shadow-lg transition-all">
       <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-1">{data.title}</h3>
-      <p className="text-sm text-gray-700 dark:text-gray-300">Status: {data.status}</p>
+      <p className={`text-sm font-medium ${getStatusColor(data.status)}`}>{data.status}</p>
       <button
         onClick={() => onViewDetail(data)}
-        className="mt-2 text-sm text-blue-600 dark:text-blue-400 hover:underline"
+        className="mt-4 text-sm text-blue-600 dark:text-blue-400 hover:underline"
       >
         View Detail
       </button>
